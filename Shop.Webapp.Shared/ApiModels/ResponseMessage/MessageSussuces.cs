86ce94cde5e06f262dc4f1138b521a72830bac8f
transytrong20 <transytrong20@gmail.com>
2023-10-31 22:34:59 +0700

    #

---
+++ Shop.Webapp.Shared/ApiModels/ResponseMessage/MessageSussuces.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.Webapp.Shared.ApiModels.ResponseMessage
+{
+    public class MessageSuccess<T>
+    {
+        public bool Success { get; set; }
+        public T Data { get; set; }
+        public string Message { get; set; }
+    }
+}
